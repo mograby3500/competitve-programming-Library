5389590f92fcfe39018b9c5a6e2516fa7e9a6eb1
Ahmad almaghraby <mograby3500@gmail.com>
2022-01-31 19:02:15 +0200

    Update dsu-kruskal.cpp

--- dsu-kruskal.cpp
+++ dsu-kruskal.cpp
@@ -1,35 +1,36 @@
 struct UnionFind{
-    vector<int> sz, parent;
-    int forests;
-
-    UnionFind(int n){
-        sz = vector<int> (n);
-        parent = vector<int> (n);
-
-        forests = n;
-        iota(parent, parent + n, 0);
-        fill(sz, sz + n, 1);
-    }
-    int find_set(int x){
-        if(x == parent[x]) return x;
-        return parent[x] = find_set(parent[x]);
-    }
-    void link(int x, int y){
-        if(sz[x] > sz[y]) swap(x, y);
-
-        parent[x] = y;
-        sz[y] += sz[x];
-    }
-    bool union_sets(int x, int y){
-        x = find_set(x);
-        y = find_set(y);
-
-        if(x != y){
-            link(x, y);
-            forests--;
-        }
-        return x != y;
-    }
+     vector<int> sz, parent;
+     int forests;
+     
+     UnionFind(int n){
+          sz = vector<int> (n);
+          parent = vector<int> (n);
+          forests = n;
+          
+          for(int i = 0; i < n; i++){
+               sz[i] = 1, parent[i] = i;
+          }
+     }
+     int find_set(int x){
+          if(x == parent[x]) return x;
+          return parent[x] = find_set(parent[x]);
+     }
+     void link(int x, int y){
+          if(sz[x] > sz[y]) swap(x, y);
+          
+          parent[x] = y;
+          sz[y] += sz[x];
+     }
+     bool union_sets(int x, int y){
+          x = find_set(x);
+          y = find_set(y);
+          
+          if(x != y){
+               link(x, y);
+               forests--;
+          }
+          return x != y;
+     }
 };
 struct edge{
     int from, to, w;
