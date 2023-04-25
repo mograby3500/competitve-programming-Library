4082aed009dc774cda8975fa964194b61ecb8a80
Ahmad almaghraby <mograby3500@gmail.com>
2023-04-25 20:25:50 +0200

    Update suffix-automaton.cpp

--- suffix-automaton.cpp
+++ suffix-automaton.cpp
@@ -1,22 +1,44 @@
-#include <bits/stdc++.h>
-using namespace std;
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize("unroll-loops")
-
 struct SuffixAutomaton{
-         vector<map<int, int>> to;
-         vector<int> link, len;
+         vector<map<char, int>> to;
+         vector<int> link, len, cnt;
          int last, sz;
+         priority_queue<pair<int,int>> pq;
          SuffixAutomaton(int n){
                   to.resize(2 * n + 10);
                   link.resize(2 * n + 10);
                   len.resize(2 * n + 10);
+                  cnt.resize(2 * n + 10);
                   last= 0, sz= 1;
          }
-         void extend(int c)
+         void calcCounts()
+         {
+                  vector<pair<int,int>> vp;
+                  for(int i = 1; i < sz; i++)
+                  {
+                           vp.push_back({-len[i], i});
+                  }
+                  sort(vp.begin(), vp.end());
+                  for(auto [L, v] : vp)
+                  {
+                           cnt[link[v]] += cnt[v];
+                  }
+         }
+         void print()
+         {
+                  for(int state= 0; state < sz; state++)
+                  {
+                           cout<<link[state]<<"\n";
+                           for(auto [c, t] : to[state])
+                           {
+                                    cout<<state<<" "<<t<<" "<<c<<"\n";
+                           }
+                  }
+         }
+         void extend(char c, int u)
          {
                   int p = last;
                   last = sz++;
+                  cnt[last]= u;
                   len[last] = len[p] + 1;
                   for (; to[p][c] == 0; p = link[p])
                            to[p][c] = last;
@@ -37,10 +59,7 @@ struct SuffixAutomaton{
                   for (; to[p][c] == q; p = link[p])
                            to[p][c] = cl;
          }
+         int nxt(int state, char c){
+                  return to[state][c];
+         }
 };
-const int N= 4e6 + 10;
-int32_t  main() {
-         cin.tie(0);
-         cin.sync_with_stdio(0);
-
-}
